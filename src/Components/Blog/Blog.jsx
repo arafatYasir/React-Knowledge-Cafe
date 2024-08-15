579df5084fbb1f8629935e148146d99cef317654
arafatYasir <arafat.yasir1305@gmail.com>
2024-08-15 20:04:05 +0600

    Added cover image and title

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title, cover} = blog;
     return (
         <div>
-            
+            <h2 className="text-2xl">{title}</h2>
+            <img className='w-[440px]' src={cover} alt="" />
         </div>
     );
 };
 
-Blog.propTypes {
+Blog.propTypes = {
     blog: PropTypes.object.isRequired
-    
-}
+};
 export default Blog;
